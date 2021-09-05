df419bc7c36f35d0b364c80e8c31380b0c952849
vfat <info@vfat.tools>
2021-09-05 23:52:11 +0100

    remove unused var

--- contracts/AdventureParty.sol
+++ contracts/AdventureParty.sol
@@ -5,7 +5,6 @@ import "./rarity.sol";
 import "./Ownable.sol";
 
 contract AdventureParty is Ownable {
-    uint public next_party;
     rarity public Rarity = rarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
     uint[] public adventurers;
 
