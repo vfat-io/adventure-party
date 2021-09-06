a241bb7aa7c97d831f87a56d5ae0860dadd4962a
vfat <info@vfat.tools>
2021-09-06 13:27:00 +0100

    Add RarityGold

--- contracts/AdventureParty.sol
+++ contracts/AdventureParty.sol
@@ -1,30 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./rarity.sol";
+import "./Rarity.sol";
+import "./RarityGold.sol";
 import "./Ownable.sol";
 
 contract AdventureParty is Ownable {
-    rarity public Rarity = rarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
+    Rarity public rarity = Rarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
+    RarityGold public rarityGold = RarityGold(0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2);
     uint[] public adventurers;
 
     constructor(uint[] memory classes) {
-        Rarity.setApprovalForAll(msg.sender, true);
+        rarity.setApprovalForAll(msg.sender, true);
         for (uint i = 0; i < classes.length; i++) {
-            adventurers.push(Rarity.next_summoner());
-            Rarity.summon(classes[i]);
+            adventurers.push(rarity.next_summoner());
+            rarity.summon(classes[i]);
         }
     }
 
     function adventureAll() external {
         for (uint i = 0; i < adventurers.length; i++) {
-            Rarity.adventure(adventurers[i]);
+            rarity.adventure(adventurers[i]);
         }
     }
 
     function levelUpAll() external {
         for (uint i = 0; i < adventurers.length; i++) {
-            Rarity.level_up(adventurers[i]);
+            rarity.level_up(adventurers[i]);
+            rarityGold.claim(adventurers[i]);
         }
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,9 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { AdventureParty__factory, Rarity } from "../typechain"
+import { AdventureParty__factory, Rarity, RarityGold } from "../typechain"
 
 describe("AdventurePaty", function () {
-  it("Should summon and level up", async function () {
+  it("Should summon", async function () {
     const AdventureParty = <AdventureParty__factory> await ethers.getContractFactory("AdventureParty");
     const adventureParty = await AdventureParty.deploy([1,2,3,4,5]);
     await adventureParty.deployed();
@@ -12,7 +12,7 @@ describe("AdventurePaty", function () {
 
     await adventureParty.adventureAll();
 
-    const rarity = <Rarity>await ethers.getContractAt("rarity", await adventureParty.Rarity());
+    const rarity = <Rarity>await ethers.getContractAt("Rarity", await adventureParty.rarity());
 
     const xp0 = parseInt(ethers.utils.formatEther(await rarity.xp(p0)));
 
@@ -22,5 +22,25 @@ describe("AdventurePaty", function () {
 
     await ethers.provider.send('evm_increaseTime', [24*60*60]);
     await adventureParty.adventureAll();
+  });
+
+  it("Should level up and claim gold", async function () {
+    const AdventureParty = <AdventureParty__factory> await ethers.getContractFactory("AdventureParty");
+    const adventureParty = await AdventureParty.deploy([1,2,3,4,5]);
+    await adventureParty.deployed();
+
+    await adventureParty.adventureAll();
+
+    for (let i = 0; i < 3; i ++) {
+      await ethers.provider.send('evm_increaseTime', [24*60*60 + 1]);
+      await adventureParty.adventureAll();
+    }
+
+    await adventureParty.levelUpAll();
+
+    const rarityGold = <RarityGold>await ethers.getContractAt("RarityGold", await adventureParty.rarityGold());
+
+    console.log("Adventurer 0 balance ", 
+      ethers.utils.formatEther(await rarityGold.balanceOf(await adventureParty.adventurers(0))));
   });
 });
