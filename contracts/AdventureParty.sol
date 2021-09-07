73a882f9c13c5897429b07a251e9c28ae4d992ba
vfat <info@vfat.tools>
2021-09-08 00:08:53 +0100

    works with unsynchronized characters, can transfer in and register new ones

--- contracts/AdventureParty.sol
+++ contracts/AdventureParty.sol
@@ -4,22 +4,32 @@ pragma solidity ^0.8.7;
 import "./Rarity.sol";
 import "./RarityGold.sol";
 import "./Ownable.sol";
+import "./EnumerableSet.sol";
 
 contract AdventureParty is Ownable, IERC721Receiver {
+    using EnumerableSet for EnumerableSet.UintSet;
+
     Rarity public rarity = Rarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
     RarityGold public rarityGold = RarityGold(0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2);
-    uint[] public adventurers;
-    uint public adventurerCount;
+    EnumerableSet.UintSet adventurers;
 
     constructor() {
         rarity.setApprovalForAll(msg.sender, true);
     }
 
+    function adventurerCount() public view returns (uint) {
+        return adventurers.length();
+    }
+
+    function adventurerAt(uint index) public view returns (uint) {
+        return adventurers.at(index);
+    }
+
     function summonMany(uint[] memory classes) external onlyOwner {
         for (uint i = 0; i < classes.length; i++) {
-            adventurers.push(rarity.next_summoner());
+            uint summonerID = rarity.next_summoner();
+            adventurers.add(summonerID);
             rarity.summon(classes[i]);
-            adventurerCount++;
         }
     }
 
@@ -30,45 +40,69 @@ contract AdventureParty is Ownable, IERC721Receiver {
     function clearParty() external {
         require(msg.sender == owner() ||
             AdventureParty(msg.sender).owner() == owner(), "Only owner or owner party");
+        for (uint i = 0; i < adventurers.length(); i++) {
+            require(rarity.ownerOf(adventurers.at(0)) != address(this), 
+                "Contract still has adventurers");
+        }
         delete adventurers;
-        adventurerCount = 0;
     }
 
     /// @dev Order of operations to transfer from party Old to party New:
     /// 1) Old.approveOtherParty(New)
     /// 2) New.transferAllFromOtherParty(Old)
+    /// *** 
+    /// Please note this does not work with the older version of this contract 
+    /// that used an array instead of an EnumerableSet. For that version please transfer
+    /// them all here manually and then call registerAdventurers()
+    /// ***
     function transferAllFromOtherParty(AdventureParty otherParty) external onlyOwner {
         uint otherPartyCount = otherParty.adventurerCount();
         for (uint i = 0; i < otherPartyCount; i++) {
-            uint adventurerID = otherParty.adventurers(i);
+            uint adventurerID = otherParty.adventurerAt(i);
             IERC721(rarity).safeTransferFrom(address(otherParty), address(this), adventurerID);
-            adventurers.push(adventurerID);
-            adventurerCount++;
+            adventurers.add(adventurerID);
         }
         otherParty.clearParty();
     }
 
     function adventureAll() external {
-        for (uint i = 0; i < adventurers.length; i++) {
-            rarity.adventure(adventurers[i]);
+        for (uint i = 0; i < adventurers.length(); i++) {
+            if (block.timestamp > rarity.adventurers_log(adventurers.at(i))) {
+                rarity.adventure(adventurers.at(i));
+            }
         }
     }
 
     function levelUpAll() external {
-        for (uint i = 0; i < adventurers.length; i++) {
-            rarity.level_up(adventurers[i]);
-            rarityGold.claim(adventurers[i]);
+        for (uint i = 0; i < adventurers.length(); i++) {
+            uint adventurer = adventurers.at(i);
+            if (rarity.xp(adventurer) > rarity.xp_required(rarity.level(adventurer))) {
+                rarity.level_up(adventurer);
+                rarityGold.claim(adventurer);
+            }
         }
     }
 
-    bytes4 constant ERC721_RECEIVED = 0x150b7a02;
+    /// @dev After transferring any summoners to this contract, call this function with their summoner IDs.
+    function registerAdventurers(uint[] memory summonerIDs) external onlyOwner {
+        for (uint i = 0; i < summonerIDs.length; i++) {
+            if (rarity.ownerOf(summonerIDs[i]) == address(this)) {
+                adventurers.add(summonerIDs[i]);
+            }
+        }
+    }
 
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata _data) 
-    public override pure returns(bytes4) {
+    // @dev After transferring out any summoners from this contract, call this function with their summoner IDs.
+    function unregisterAdventurers(uint[] memory summonerIDs) external onlyOwner {
+        for (uint i = 0; i < summonerIDs.length; i++) {
+            if (rarity.ownerOf(summonerIDs[i]) != address(this)) {
+                adventurers.remove(summonerIDs[i]);
+            }
+        }
+    }
+
+    function onERC721Received(address, address, uint256, bytes calldata) 
+            public override pure returns(bytes4) {
         return this.onERC721Received.selector;
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 import { AdventureParty__factory, Rarity, RarityGold } from "../typechain"
 
 describe("AdventureParty", function () {
-  it("Should summon", async function () {
+  it("Should summon", async () => {
     const AdventureParty = <AdventureParty__factory> await ethers.getContractFactory("AdventureParty");
     const adventureParty = await AdventureParty.deploy();
     await adventureParty.deployed();
@@ -11,17 +11,21 @@ describe("AdventureParty", function () {
     await adventureParty.adventureAll();
 
     const rarity = <Rarity>await ethers.getContractAt("Rarity", await adventureParty.rarity());
-    const p0 = await adventureParty.adventurers(0);
+    const p0 = await adventureParty.adventurerAt(0);
     const xp0 = parseInt(ethers.utils.formatEther(await rarity.xp(p0)));
     expect(xp0).to.be.equal(250, "XP should be 250 after adventuring");
     
-    await expect(adventureParty.adventureAll()).to.be.revertedWith("");
-
-    await ethers.provider.send('evm_increaseTime', [24*60*60]);
     await adventureParty.adventureAll();
+    const xp1 = parseInt(ethers.utils.formatEther(await rarity.xp(p0)));
+    expect(xp1).to.be.equal(250, "XP should be 250 after adventuring twice in a row");
+
+    await ethers.provider.send('evm_increaseTime', [24*60*60 + 1000]);
+    await adventureParty.adventureAll();
+    const xp2 = parseInt(ethers.utils.formatEther(await rarity.xp(p0)));
+    expect(xp2).to.be.equal(500, "XP should be 500 after adventuring for two days");
   });
 
-  it("Should level up and claim gold", async function () {
+  it("Should level up and claim gold", async () => {
     const AdventureParty = <AdventureParty__factory> await ethers.getContractFactory("AdventureParty");
     const adventureParty = await AdventureParty.deploy();
     await adventureParty.deployed();
@@ -29,7 +33,7 @@ describe("AdventureParty", function () {
     await adventureParty.adventureAll();
 
     for (let i = 0; i < 3; i ++) {
-      await ethers.provider.send('evm_increaseTime', [24*60*60 + 1]);
+      await ethers.provider.send('evm_increaseTime', [24*60*60 + 1000]);
       await adventureParty.adventureAll();
     }
 
@@ -38,10 +42,10 @@ describe("AdventureParty", function () {
     const rarityGold = <RarityGold>await ethers.getContractAt("RarityGold", await adventureParty.rarityGold());
 
     console.log("Adventurer 0 balance ", 
-      ethers.utils.formatEther(await rarityGold.balanceOf(await adventureParty.adventurers(0))));
+      ethers.utils.formatEther(await rarityGold.balanceOf(await adventureParty.adventurerAt(0))));
   });
 
-  it("Should transfer all from one party to another", async function () {
+  it("Should transfer all from one party to another", async () => {
     const AdventureParty = <AdventureParty__factory> await ethers.getContractFactory("AdventureParty");
     const adventureParty = await AdventureParty.deploy();
     await adventureParty.deployed();
@@ -54,5 +58,20 @@ describe("AdventureParty", function () {
 
     console.log("Adventure Party 1 count", (await adventureParty.adventurerCount()).toNumber());
     console.log("Adventure Party 2 count", (await adventureParty2.adventurerCount()).toNumber());
+  });
+
+  it("Should summon on wallet, transfer into party, register on party, and level up", async () => {
+    const AdventureParty = <AdventureParty__factory> await ethers.getContractFactory("AdventureParty");
+    const adventureParty = await AdventureParty.deploy();
+    await adventureParty.deployed();
+    const rarity = <Rarity>await ethers.getContractAt("Rarity", await adventureParty.rarity());
+    const nextSummoner = await rarity.next_summoner();
+    await rarity.summon(1);
+    await rarity.transferFrom(await adventureParty.owner(), adventureParty.address, nextSummoner);
+    await adventureParty.registerAdventurers([nextSummoner]);
+    expect((await adventureParty.adventurerCount()).toNumber()).to.equal(1);
+    await adventureParty.adventureAll();
+    const xp = parseInt(ethers.utils.formatEther(await rarity.xp(nextSummoner)));
+    expect(xp).to.be.equal(250, "XP should be 250 after adventuring");
   })
 });
